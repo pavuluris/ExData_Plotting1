d55bb126bfe41dc2f446458fefe420b76aef450e
pavuluris <pavuluris@gmail.com>
2016-01-10 16:18:36 -0800

    legend

--- Plot3.R
+++ Plot3.R
@@ -2,7 +2,7 @@ plot3 <- function() {
   plot(df$timestamp,df$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
   lines(df$timestamp,df$Sub_metering_2,col="red")
   lines(df$timestamp,df$Sub_metering_3,col="blue")
-  legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=2,text.font=2,cex=0.64)
+  legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=2,text.font=2,cex=0.5)
   dev.copy(png, file="plot3.png", width=500, height=500)
   dev.off()
   cat("plot3.png has been saved in", getwd())
