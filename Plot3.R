893771c672ca9be2d5fd2e0e7976065972df5e40
pavuluris <pavuluris@gmail.com>
2016-01-10 16:15:17 -0800

    changed teh font for legend

--- Plot3.R
+++ Plot3.R
@@ -2,8 +2,8 @@ plot3 <- function() {
   plot(df$timestamp,df$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
   lines(df$timestamp,df$Sub_metering_2,col="red")
   lines(df$timestamp,df$Sub_metering_3,col="blue")
-  legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=2)
-  dev.copy(png, file="plot3.png", width=800, height=500)
+  legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=2,text.font=2,cex=0.64)
+  dev.copy(png, file="plot3.png", width=500, height=500)
   dev.off()
   cat("plot3.png has been saved in", getwd())
 }
