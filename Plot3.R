4ab325e788561b3c634c17a0c678e4074a6f00ce
pavuluris <pavuluris@gmail.com>
2016-01-10 16:30:49 -0800

    legend fix

--- Plot3.R
+++ Plot3.R
@@ -2,8 +2,9 @@ plot3 <- function() {
   plot(df$timestamp,df$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
   lines(df$timestamp,df$Sub_metering_2,col="red")
   lines(df$timestamp,df$Sub_metering_3,col="blue")
-  legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=2,text.font=2,cex=0.5)
-  dev.copy(png, file="plot3.png", width=500, height=500)
+  legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),
+     lwd=c(1,1),cex=0.5)
+  dev.copy(png, file="plot3.png", width=800, height=600)
   dev.off()
   cat("plot3.png has been saved in", getwd())
 }
